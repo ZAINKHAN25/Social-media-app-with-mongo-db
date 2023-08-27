ed98429a5ac8c20dc31db0ccf57019be74109a18
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-08-27 17:50:47 +0500

    create, update, Delete is done

--- routers/auth.js
+++ routers/auth.js
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt"
 
 var authroutes = express.Router();
 
+// Signup 
 authroutes.post('/register', async (req, res)=>{
     try {
         // genereate new password
@@ -19,9 +20,28 @@ authroutes.post('/register', async (req, res)=>{
         const user = await newUser.save()
         res.status(200).json(user)
     } catch (error) {
-        console.log(error);
+        res.status(500).json(error)
     }
 })
 
+
+// Login
+authroutes.post('/login', async (req, res)=>{
+    try {
+        const user = await User.findOne({email: req.body.email})
+        if(!user){
+            res.status(404).json("User not found")
+        }
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        console.log(validPassword);
+        if(!validPassword){
+            res.status(400).json("Wrong Password")
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 export default authroutes;
--- routers/user.js
+++ routers/user.js
@@ -1,12 +1,52 @@
-import  express  from "express"; 
+import express from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+
 
 var userroutes = express.Router();
 
-userroutes.get('/', (req, res)=>{
-    res.send("You are in home page")
+// update user
+userroutes.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt)
+            } catch (error) {
+                return res.status(500).json(error)
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+                })
+                res.status(200).json("Account has been updated")
+        }
+        catch(error){
+            return res.status(500).json(error)
+        }
+    } else {
+        return res.status(403).json("You can update your account only")
+    }
 })
-userroutes.get('/about', (req, res)=>{
-    res.send("You are in about page")
+
+
+// delete a user
+userroutes.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+                await User.findByIdAndDelete(req.params.id)
+                res.status(200).json("Account has been delete")
+        }
+        catch(error){
+            return res.status(500).json(error)
+        }
+    } else {
+        return res.status(403).json("You can delete your account only")
+    }
 })
+// get a user 
+// follow a user 
+// unfollow a user 
 
 export default userroutes;
