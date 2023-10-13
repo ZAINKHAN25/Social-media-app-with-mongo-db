2ae0f963090afce851b359ebdfaa46f16f20da40
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-13 07:51:43 -0700

    kam gogya

--- routers/posts.js
+++ routers/posts.js
@@ -9,7 +9,7 @@ postRoute.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
-    res.status(200).json(savedPost.reverse());
+    res.status(200).json(savedPost);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -18,7 +18,7 @@ postRoute.post("/", async (req, res) => {
 postRoute.get("/", async (req, res) => {
   try {
     const posts = await Post.find({});
-    res.status(200).json(posts);
+    res.status(200).json(posts.reverse());
   } catch (err) {
     res.status(500).json(err);
   }
