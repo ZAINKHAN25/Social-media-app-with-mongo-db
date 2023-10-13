c7735b183118c0ea9990cab9fe8009cd0a32eaf3
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2023-10-13 07:23:28 -0700

    ulta kar diya post ki api

--- routers/posts.js
+++ routers/posts.js
@@ -9,7 +9,7 @@ postRoute.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
     const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
+    res.status(200).json(savedPost.reverse());
   } catch (err) {
     res.status(500).json(err);
   }
